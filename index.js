0311fc7eeb6260f6bac778d308e1053fad497100
Phelix <phelixmbani@gmail.com>
2024-10-04 16:17:55 +0300

    DOM elements

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const mainId = document.getElementById("main");
+mainId.remove()
+
+const newHeader = document.createElement("h1")
+
+newHeader.id = "victory";  
+newHeader.textContent = "Phyl is the champion"
+document.body.appendChild(newHeader)
+
+
